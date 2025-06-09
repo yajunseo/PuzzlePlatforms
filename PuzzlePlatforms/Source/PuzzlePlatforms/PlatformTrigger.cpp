534761ad9ba5a3430056f117855cc5c6d87afa1f
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2025-06-10 00:33:04 +0900

    발판 충돌 추가

--- PuzzlePlatforms/Source/PuzzlePlatforms/PlatformTrigger.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/PlatformTrigger.cpp
@@ -22,7 +22,9 @@ APlatformTrigger::APlatformTrigger()
 void APlatformTrigger::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapBegin);
+	TriggerVolume->OnComponentEndOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapEnd);
 }
 
 // Called every frame
@@ -30,5 +32,23 @@ void APlatformTrigger::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void APlatformTrigger::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+	class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (HasAuthority())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("APlatformTrigger::OnOverlapBegin"));
+	}
+}
+
+void APlatformTrigger::OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+	class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (HasAuthority())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("APlatformTrigger::OnOverlapEnd"));
+	}
 }
 
